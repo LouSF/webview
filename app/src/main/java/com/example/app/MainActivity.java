b4aaa5999c6b18a01c62f293f089fa77da8a4ac3
lsf <1810488677@qq.com>
2024-04-27 14:51:00 +0800

    c

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         mWebView.setWebViewClient(new MyWebViewClient());
 
         // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
+         mWebView.loadUrl("http://10.12.4.192:3002");
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
